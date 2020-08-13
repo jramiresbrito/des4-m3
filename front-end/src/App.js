7f6ce2e84a95f3477163b13d3c57d6c91dbd9caa
Joao Victor Ramires Guimaraes Brito <joaoramires.engsoft@gmail.com>
2020-08-13 14:30:05 -0300

    ADD Spinner

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as api from './service/gradesService';
+import Spinner from './components/Spinner';
 
 export default function App() {
-  const testApi = async() => {
-    const result = await api.getAllGrades();
-    console.log(result);
-  }
+  const [allGrades, setAllGrades] = useState([]);
+  const [selectedGrade, setSelectedGrade] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  testApi();
+  useEffect(() => {
+    const getGrades = async () => {
+      const grades = await api.getAllGrades();
+      setTimeout(() => {
+        setAllGrades(grades);
+      }, 1500);
+    }
+    getGrades();
+  }, []);
+
   return (
-    <>
-      <p>hello, world!!!</p>
-    </>
+    <div>
+      <h1 className="center">Controle de Notas</h1>
+      {allGrades.length > 0 && <p>Notas Disponíveis</p>}
+      {allGrades.length === 0 && <Spinner/>}
+    </div>
   );
 }
